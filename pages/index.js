8312c928117d4e8c99a9e8e036fe2112280f5209
alex <alextechnology9@gmail.com>
2022-02-16 16:17:44 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
+
+export default function Home({ results }) {
+  return (
+    <div>
+      <Head>
+        <title>Hulu 2.0</title>
+        <link rel="icon" href="/Favicon.ico" />
+      </Head>
+
+      <Header />
+      <Nav />
+      <Results results={results} />
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+  return {
+    props: {
+      results: request.results
+    }
+  };
+}
